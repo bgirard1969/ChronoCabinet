f9a0b7755e50919d3e6d2ef3d90989f6da215358
bgirard1969 <bgirard1969@gmail.com>
2026-04-19 22:31:15 -0400

    Remove React frontend and Python backend, keep Angular stack only

--- backend-python/sio.py
+++
@@ -1,8 +0,0 @@
-import socketio
-
-sio = socketio.AsyncServer(async_mode="asgi", cors_allowed_origins="*")
-
-
-async def emit_event(event: str, data: dict = None):
-    """Emit a Socket.IO event to all connected clients."""
-    await sio.emit(event, data or {})
